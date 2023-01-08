e1407ae30bc26106b0e2ab3887bc1ab5494293fb
shadat rahman <shadat.rahman.464@gmail.com>
2023-01-08 22:51:29 +0600

    step3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
 
 void main() {
   runApp(const MyApp());
@@ -22,34 +31,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const url =
+    "https://images.unsplash.com/photo-1532170579297-281918c8ae72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1184&q=80";
+
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
-    );
+    final future = useMemoized(
+        () => NetworkAssetBundle(Uri.parse(url)).load(url).then((value) => value.buffer.asUint8List()).then((value) => Image.memory(value)));
+    final snapshot = useFuture(future);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Home Page"),
       ),
       body: Column(
-        children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('You typed ${text.value}'),
-        ],
+        children: [snapshot.data].compactMap().toList(),
       ),
     );
   }
