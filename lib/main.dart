611ec86c8ce4b6b21362f782e23eac6eb8ef048a
shadat rahman <shadat.rahman.464@gmail.com>
2023-01-08 21:55:15 +0600

    step-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,17 +21,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+Stream<String> getTime() => Stream.periodic(
+      const Duration(
+        seconds: 1,
+      ),
+      (_) => DateTime.now().toIso8601String(),
+    );
+
+class HomePage extends HookWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
 
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+  final dateTime = useStream(getTime());
     return Scaffold(
+      appBar: AppBar(
+        title: Text(dateTime.data ?? "Home Page"),
+      ),
       body: Container(),
     );
   }
