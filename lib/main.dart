a83307e2b7d087d6fc46f2909ed3597ba9c72c6d
shadat rahman <shadat.rahman.464@gmail.com>
2023-01-11 18:55:00 +0600

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -31,24 +33,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const url =
-    "https://images.unsplash.com/photo-1532170579297-281918c8ae72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1184&q=80";
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(
+      const Duration(seconds: 1),
+      (v) => from - v,
+    ).takeWhile((value) => value >= 0).listen((value) {
+      this.value = value;
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final future = useMemoized(
-        () => NetworkAssetBundle(Uri.parse(url)).load(url).then((value) => value.buffer.asUint8List()).then((value) => Image.memory(value)));
-    final snapshot = useFuture(future);
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Home Page"),
       ),
       body: Column(
-        children: [snapshot.data].compactMap().toList(),
+        children: [
+          Text(notifier.value.toString()),
+        ],
       ),
     );
   }
